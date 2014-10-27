eafb84a4380a7d3c685fb9b2e13a01385ef4f870
Joshua Skinner <jkyuusai@gmail.com>
2014-10-26 22:38:48 -0400

    WIP COMMIT #16

--- client/feelings/feelingSection.js
+++ client/feelings/feelingSection.js
@@ -15,6 +15,34 @@ Template.feelingSection.helpers({
 });
 
 Template.feelingItem.helpers({
+  color: function(s) {
+  	//TODO: fix bug in #16
+	var node = root.first(function(n){ return n.model.id === s }),
+		path,
+		distanceFromPrimary;
+
+	console.log('color');
+		
+
+  	if(!node) {
+  		return;
+  	}
+
+  	path = node.getPath();
+
+  	if(!path) {
+  		return;
+  	}
+
+  	distanceFromPrimary = path.length - 2;
+
+  	if(distanceFromPrimary) {
+  		return path[1].model.id + '-child-' + distanceFromPrimary;
+  	}
+
+  	return node.model.id;
+  },
+
   name: function() {
   	return upperCase(this);
   }
